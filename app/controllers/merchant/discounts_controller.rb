8a34236c73b81eaf3a2b2d557924fd6401bb4acf
Maxwell Baird <bairdm71@gmail.com>
2020-04-21 15:49:57 -0600

    created the view for discount create

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -7,4 +7,22 @@ class Merchant::DiscountsController < Merchant::BaseController
   def new
     @merchant = Merchant.find(current_user[:merchant_id])
   end
+
+  def create
+    @merchant = Merchant.find(current_user[:merchant_id])
+    discount = @merchant.discounts.new(discount_params)
+    if discount.save
+      flash[:success] = "A new discount has created."
+      redirect_to "/merchant/discounts"
+    else
+      flash[:error] = "Unable to create discount: #{discount.errors.full_messages.to_sentence}."
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:percent,:amount)
+  end
 end
