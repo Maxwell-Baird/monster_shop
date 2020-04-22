ac403474a856e0e497533c17a1566ba44a903cdc
Maxwell Baird <bairdm71@gmail.com>
2020-04-21 20:42:57 -0600

    change the parameters of the percent

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,5 +2,5 @@ class Discount < ApplicationRecord
   belongs_to :merchant
   validates_presence_of :percent, :amount
 
-  validates_inclusion_of :percent, in: 1..100
+  validates_inclusion_of :percent, in: 1..99
 end
