2ab6103ddfcb422a952890c110d5e4d00b74c2d2
Maxwell Baird <bairdm71@gmail.com>
2020-04-21 20:44:13 -0600

    added validate presence of for discount

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -7,4 +7,9 @@ describe Discount, type: :model do
     it {should belong_to :merchant}
   end
 
+  describe "validations" do
+    it { should validate_presence_of :percent }
+    it { should validate_presence_of :amount }
+  end
+
 end
